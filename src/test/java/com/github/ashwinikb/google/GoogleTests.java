8eb7eea49403f7e993e62007a59312e1282f2674
“Ashwini <ashwini.kbasavaraju@gmail.com>
2019-01-08 20:46:46 -0800

    Adding google search test

---
+++ src/test/java/com/github/ashwinikb/google/GoogleTests.java
@@ -0,0 +1,32 @@
+package com.github.ashwinikb.google;
+
+import com.github.ashwinikb.DriverSetUp;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+public class GoogleTests {
+
+    public WebDriver driver;
+
+    @BeforeClass(alwaysRun = true)
+    @Parameters({"os", "browser", "url", "node"})
+    public void setUp(String os, String browser, String url, String node) throws MalformedURLException {
+        DriverSetUp setupTestDriver = new DriverSetUp(os, browser, url, node);
+        driver = setupTestDriver.getDriver();
+    }
+
+    @Test(priority = 1)
+    public void search() {
+        driver.get("https://www.google.com");
+        driver.findElement(By.id("lst-ib")).sendKeys("Selenium");
+        driver.findElement(By.name("btnK")).click();
+        driver.manage().window().maximize();
+
+    }
+
+}
